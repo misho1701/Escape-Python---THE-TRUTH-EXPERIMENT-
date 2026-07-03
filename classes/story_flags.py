1ff542d96fddb30219701ef6eaaea21ff7c60428
Mihail Georgiev <115742050+misho1701@users.noreply.github.com>
2026-07-03 22:46:41 +0300

    Delete classes/story_flags.py

--- classes/story_flags.py
+++
@@ -1,9 +0,0 @@
-class Flags:
-    def __init__(self):
-        self.flags = {}
-
-    def set(self, key, value=True):
-        self.flags[key] = value
-
-    def get(self, key):
-        return self.flags.get(key, False)
